2d689886c5122612d8052580c1eaa6d8f1a1f40e
Cepblu-tyt <sergei.oksanen@gmail.com>
2023-02-13 19:06:32 +0200

    exercise-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TeacherComponent } from './teacher/teacher.component';
 import { StudentComponent } from './student/student.component';
+import { StudentFormComponent } from './student-form/student-form.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     TeacherComponent,
-    StudentComponent
+    StudentComponent,
+    StudentFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

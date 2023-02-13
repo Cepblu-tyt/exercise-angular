c36743c92f52c40581eb9d0b7a5bd0142725c796
Cepblu-tyt <sergei.oksanen@gmail.com>
2023-02-13 21:36:36 +0200

    exercise-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TeacherComponent } from './teacher/teacher.component';
 import { StudentComponent } from './student/student.component';
+import { TeacherFormComponent } from './teacher-form/teacher-form.component';
 import { StudentFormComponent } from './student-form/student-form.component';
 import {FormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     TeacherComponent,
     StudentComponent,
+    TeacherFormComponent,
     StudentFormComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/teacher/teacher.component.spec.ts
+++ src/app/teacher/teacher.component.spec.ts
@@ -1,18 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TeacherComponent } from './teacher.component';
+// @ts-ignore
+import { TeacherFormComponent } from '../teacher-form.component';
 
-describe('TeacherComponent', () => {
-  let component: TeacherComponent;
-  let fixture: ComponentFixture<TeacherComponent>;
+describe('TeacherFormComponent', () => {
+  let component: TeacherFormComponent;
+  let fixture: ComponentFixture<TeacherFormComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TeacherComponent ]
+      declarations: [ TeacherFormComponent ]
     })
       .compileComponents();
 
-    fixture = TestBed.createComponent(TeacherComponent);
+    fixture = TestBed.createComponent(TeacherFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
